1dd917a09488440284bcd866b56c12690e129a81
3larvas <3larvas@gmail.com>
2020-02-24 00:37:44 +0900

    3190 뱀 문제 풀이 완료 방향 변수 음수로 변하는지 항상 확인!

--- algorithm_test_/algorithm_test_/algo_3190_뱀.cpp
+++ algorithm_test_/algorithm_test_/algo_3190_뱀.cpp
@@ -1,6 +1,7 @@
 // 출처 : https://www.acmicpc.net/problem/3190
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
 struct snk {
@@ -9,7 +10,7 @@ struct snk {
 	int dir = 0;
 };
 int n;
-int map[101][101];
+bool map[101][101];
 queue<pair<int, bool>> turn_info;
 
 int main() {
@@ -18,30 +19,29 @@ int main() {
 	cin >> n;
 	int cnt = 0;
 	cin >> cnt;
+	int  a = 0;
+	int  b = 0;
+	int  c = 0;
+	char d = 0;
 	for (int i = 0; i < cnt; i++) {
-		int a = 0;
-		int b = 0;
 		cin >> a >> b;
 		map[a][b] = 1;
 	}
 	cin >> cnt;
 	for (int i = 0; i < cnt; i++) {
-		int a = 0;
-		char b = 0;
-		cin >> a >> b;
-		if (b == 'D') turn_info.push(make_pair(a, true));
-		else turn_info.push(make_pair(a, false));		
+		cin >> c >> d;
+		if (d == 'D') turn_info.push(make_pair(c, true));
+		else turn_info.push(make_pair(c, false));
 	}
 	snake.snk_pos.push_back(make_pair(1, 1));
-	
-	while(true){
-		snake.cnt += 1;
+
+	while (true) {
 		int nxt_head_col = snake.snk_pos.back().first;
 		int nxt_head_row = snake.snk_pos.back().second;
-
-		switch (snake.dir%4) {
+		snake.cnt += 1;
+		switch (snake.dir % 4) { // 이부분이 문제...!!! dir이 음수일수도 있었다.
 		case 0:
-			nxt_head_col += 1; 
+			nxt_head_col += 1;
 			break;
 		case 1:
 			nxt_head_row += 1;
@@ -53,9 +53,9 @@ int main() {
 			nxt_head_row -= 1;
 			break;
 		}
-		
+
 		for (int i = 0; i < snake.snk_pos.size(); i++) {
-			if (nxt_head_col == snake.snk_pos.at(i).first 
+			if (nxt_head_col == snake.snk_pos.at(i).first
 				&& nxt_head_row == snake.snk_pos.at(i).second) {
 				cout << snake.cnt;
 				chk = true;
@@ -74,7 +74,10 @@ int main() {
 		if (!turn_info.empty()) {
 			if (snake.cnt == turn_info.front().first) {
 				if (turn_info.front().second == true) snake.dir += 1;
-				else snake.dir -= 1;
+				else { /// 여기서 dir이 음수일때 처리!
+					if (snake.dir == 0) snake.dir = 3;
+					else  snake.dir -= 1;
+				}
 				turn_info.pop();
 			}
 		}
