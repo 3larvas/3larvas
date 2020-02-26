8596a1063eb738803408ceca01de56c416e1209c
3larvas <3larvas@gmail.com>
2020-02-27 02:05:28 +0900

    테트로미노 풀이중

--- algorithm_test_/algorithm_test_/algo_14500_테트로미노.cpp
+++ algorithm_test_/algorithm_test_/algo_14500_테트로미노.cpp
@@ -1,10 +1,10 @@
 //√‚√≥ : https://www.acmicpc.net/problem/14500
 #include <iostream>
 using namespace std;
+int map[501][501];
 
 int main() {
 	int n, m;
-	int map[501][501] = { 0, };
 	cin >> n >> m;
 	int input;
 	for (int i = 0; i < n; i++) {
@@ -13,16 +13,52 @@ int main() {
 			map[i][j] = input;
 		}
 	}
-	return 0;
-	for (int i = 0; i < 5; i++) {
-		switch (i) {
+	int result = 0;
+	for (int mode = 0; mode < 5; mode++) {
+		switch (mode) {
 		case 0:
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j + 3 < m; j++) {
+					int tmp_result = 0;
+					for (int k = j; k < j + 4; k++) tmp_result += map[i][k];
+					if (tmp_result > result) result = tmp_result;
+				}
+			}
 			break;
 		case 1:
+			for (int i = 0; i + 3 < n; i++) {
+				for (int j = 0; j < m; j++) {
+					int tmp_result = 0;
+					for (int k = i; k < i + 4; k++) tmp_result += map[k][j];
+					if (tmp_result > result) result = tmp_result;
+				}
+			}
 			break;
 		case 2:
+			for (int i = 0; i + 1 < n; i++) {
+				for (int j = 0; j + 1 < m; j++) {
+					int tmp_result = 0;
+					for (int k = i; k < i + 2; k++) {
+						for (int l = j; l < j + 2; l++) {
+							tmp_result += map[k][l];
+						}
+					}
+					if (tmp_result > result) result = tmp_result;
+				}
+			}
 			break;
 		case 3:
+			for (int i = 0; i + 1 < n; i++) {
+				for (int j = 0; j + 1 < m; j++) {
+					int tmp_result = 0;
+					for (int k = i; k < i + 2; k++) {
+						for (int l = j; l < j + 3; l++) {
+							tmp_result += map[k][l];
+						}
+					}
+					if (tmp_result > result) result = tmp_result;
+				}
+			}
 			break;
 		case 4:
 			break;
@@ -30,4 +66,6 @@ int main() {
 			break;
 		}
 	}
+	cout << result;
+	return 0;
 }
