4947aaa21a2cc248731fb60284a5797c1053198f
3larvas <3larvas@gmail.com>
2020-02-27 02:03:04 +0900

    테트로미노 풀이시작

---
+++ algorithm_test_/algorithm_test_/algo_14500_테트로미노.cpp
@@ -0,0 +1,33 @@
+//√‚√≥ : https://www.acmicpc.net/problem/14500
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, m;
+	int map[501][501] = { 0, };
+	cin >> n >> m;
+	int input;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> input;
+			map[i][j] = input;
+		}
+	}
+	return 0;
+	for (int i = 0; i < 5; i++) {
+		switch (i) {
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			break;
+		}
+	}
+}
