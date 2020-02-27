08417bd58b33f5785e7132ad54960ca77cdbf1ac
3larvas <3larvas@gmail.com>
2020-02-27 12:59:02 +0900

    테트로미노 풀이완료

--- algorithm_test_/algorithm_test_/algo_14500_테트로미노.cpp
+++ algorithm_test_/algorithm_test_/algo_14500_테트로미노.cpp
@@ -4,13 +4,14 @@ using namespace std;
 int map[501][501];
 
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 	int n, m;
 	cin >> n >> m;
-	int input;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			cin >> input;
-			map[i][j] = input;
+			cin >> map[i][j];
 		}
 	}
 	int result = 0;
@@ -50,19 +51,90 @@ int main() {
 		case 3:
 			for (int i = 0; i + 1 < n; i++) {
 				for (int j = 0; j + 1 < m; j++) {
-					int tmp_result = 0;
+					int tmp_arry[6] = { 0, };
+					int cnt = 0;
 					for (int k = i; k < i + 2; k++) {
 						for (int l = j; l < j + 3; l++) {
-							tmp_result += map[k][l];
+							tmp_arry[cnt] = map[k][l];
+							cnt++;
 						}
 					}
-					if (tmp_result > result) result = tmp_result;
+					for (int i = 0; i < 8; i++) {
+						int tmp_result = 0;
+						switch (i) {
+						case 0:
+							tmp_result = tmp_arry[2] + tmp_arry[3] + tmp_arry[4] + tmp_arry[5];
+							break;
+						case 1:
+							tmp_result = tmp_arry[0] + tmp_arry[3] + tmp_arry[4] + tmp_arry[5];
+							break;
+						case 2:
+							tmp_result = tmp_arry[0] + tmp_arry[1] + tmp_arry[2] + tmp_arry[5];
+							break;
+						case 3:
+							tmp_result = tmp_arry[0] + tmp_arry[1] + tmp_arry[2] + tmp_arry[3];
+							break;
+						case 4:
+							tmp_result = tmp_arry[1] + tmp_arry[2] + tmp_arry[3] + tmp_arry[4];
+							break;
+						case 5:
+							tmp_result = tmp_arry[0] + tmp_arry[1] + tmp_arry[4] + tmp_arry[5];
+							break;
+						case 6:
+							tmp_result = tmp_arry[0] + tmp_arry[1] + tmp_arry[2] + tmp_arry[4];
+							break;
+						case 7:
+							tmp_result = tmp_arry[1] + tmp_arry[3] + tmp_arry[4] + tmp_arry[5];
+							break;
+						}
+						if (tmp_result > result) result = tmp_result;
+					}				
 				}
 			}
 			break;
 		case 4:
-			break;
-		case 5:
+			for (int i = 0; i + 1 < n; i++) {
+				for (int j = 0; j + 1 < m; j++) {
+					int tmp_arry[6] = { 0, };
+					int cnt = 0;
+					for (int k = i; k < i + 3; k++) {
+						for (int l = j; l < j + 2; l++) {
+							tmp_arry[cnt] = map[k][l];
+							cnt++;
+						}
+					}
+					for (int i = 0; i < 8; i++) {
+						int tmp_result = 0;
+						switch (i) {
+						case 0:
+							tmp_result = tmp_arry[1] + tmp_arry[3] + tmp_arry[4] + tmp_arry[5];
+							break;
+						case 1:
+							tmp_result = tmp_arry[0] + tmp_arry[2] + tmp_arry[4] + tmp_arry[5];
+							break;
+						case 2:
+							tmp_result = tmp_arry[0] + tmp_arry[1] + tmp_arry[2] + tmp_arry[4];
+							break;
+						case 3:
+							tmp_result = tmp_arry[0] + tmp_arry[1] + tmp_arry[3] + tmp_arry[5];
+							break;
+						case 4:
+							tmp_result = tmp_arry[0] + tmp_arry[2] + tmp_arry[3] + tmp_arry[5];
+							break;
+						case 5:
+							tmp_result = tmp_arry[1] + tmp_arry[2] + tmp_arry[3] + tmp_arry[4];
+							break;
+						case 6:
+							tmp_result = tmp_arry[0] + tmp_arry[2] + tmp_arry[3] + tmp_arry[4];
+							break;
+						case 7:
+							tmp_result = tmp_arry[1] + tmp_arry[2] + tmp_arry[3] + tmp_arry[5];
+							break;
+						}
+						if (tmp_result > result) result = tmp_result;
+					}
+				}
+			}
 			break;
 		}
 	}
