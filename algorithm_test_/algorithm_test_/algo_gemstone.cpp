de5020748078df49dbb2498c37acd7f5a05fe38f
3larvas <3larvas@gmail.com>
2020-03-25 12:10:12 +0900

    연습문제

--- algorithm_test_/algorithm_test_/algo_gemstone.cpp
+++ algorithm_test_/algorithm_test_/algo_gemstone.cpp
@@ -1,14 +1,28 @@
-#pragma warning (disable : 4996)
-#include <cstdio>
-
-int n;
-char arr[100][100];
-
-int main() {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-		scanf("%s", arr[i]);
-	printf("");
-
-	return 0;
-}
+//#pragma warning (disable : 4996)
+//#include <cstdio>
+//
+//int n;
+//char arr[100][100];
+//bool vis[30];
+//int memo[30];
+//int result;
+//
+//int main() {
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++)
+//		scanf("%s", arr[i]);
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < 100; j++) {
+//			if (arr[i][j] == 0) break;
+//			if (!vis[(int)arr[i][j] - 97]) {
+//				vis[(int)arr[i][j] - 97] = true;
+//				memo[(int)arr[i][j] - 97] += 1;
+//			}
+//		}
+//		for (int j = 0; j < 30; j++) vis[j] = false;
+//	}
+//	for (int j = 0; j < 30; j++)
+//		if (memo[j] == n) result++;
+//	printf("%d", result);
+//	return 0;
+//}
---
+++ algorithm_test_/algorithm_test_/algo_intersect.cpp
@@ -0,0 +1,13 @@
+#pragma warning(disable : 4996)
+#include <cstdio>
+
+int n;
+int lines[100][4];
+
+int main() {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) 
+		scanf("%d %d %d %d", &lines[i][0], &lines[i][1], &lines[i][2], &lines[i][3]);
+
+	return 0;
+}
