a1b9e72198fc1587f76ea0fdffc94511d49b98f1
3larvas <3larvas@gmail.com>
2020-03-28 11:29:42 +0900

    기타리스트 풀이완료 안녕 dp풀이

--- algorithm_test_/algorithm_test_/algo_1495_기타리스트.cpp
+++ algorithm_test_/algorithm_test_/algo_1495_기타리스트.cpp
@@ -1,1 +1,32 @@
-// 출처 : https://www.acmicpc.net/problem/1495
+//// 출처 : https://	www.acmicpc.net/problem/1495
+//#pragma warning (disable : 4996)
+//#include <cstdio>
+//
+//int n, m, s;
+//bool dp[101][1100];
+//int v[101];
+//int max;
+//bool chk;
+//
+//int main() {
+//	scanf("%d %d %d", &n, &s, &m);
+//	for (int i = 0; i < n; i++) scanf("%d", &v[i]);
+//	dp[0][s] = true;
+//	for (int i = 0; i < n; i < i++) {
+//		for (int j = 0; j <= m; j++) {
+//			if (dp[i][j]) {
+//				if (j - v[i] >= 0) dp[i + 1][j - v[i]] = true;
+//				if (j + v[i] <= m) dp[i + 1][j + v[i]] = true;
+//			}
+//		}
+//	}
+//	for (int i = 0; i <= 1000; i++) {
+//		if (dp[n][i]) {
+//			if (max < i) max = i;
+//			chk = true;
+//		}
+//	}
+//	if (chk) printf("%d", max);
+//	else printf("-1");
+//	return 0;
+//}
--- algorithm_test_/algorithm_test_/algo_1535_안녕.cpp
+++ algorithm_test_/algorithm_test_/algo_1535_안녕.cpp
@@ -29,3 +29,33 @@
 //	printf("%d", love);
 //	return 0;
 //}
+//√‚√≥ : https://www.acmicpc.net/problem/1535
+#pragma warning (disable : 4996)
+#include <cstdio>
+
+int n;
+int bad[20];
+int good[20];
+bool dp[20][100];
+int result;
+
+int main() {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) scanf("%d", &bad[i]);
+	for (int i = 0; i < n; i++) scanf("%d", &good[i]);
+	dp[0][100] = true;
+	for (int i = 0; i < n; i++) {
+		for (int j = 1; j <= 100 ; j++) {
+			if (dp[i][j]) {
+				if (j - bad[i] >= 0) dp[i + 1][j - bad[i]] = true;
+			}
+		}
+	}
+	for (int i = 0; i < 100; i++) {
+		if (dp[n][i]) {
+			if (result < i) result = i;
+		}
+	}
+	printf("%d", result);
+	return 0;
+}
