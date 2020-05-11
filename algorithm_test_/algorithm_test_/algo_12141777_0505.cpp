ea1747b07789367b2ee846e465964b93f2d47da6
3larvas <3larvas@gmail.com>
2020-05-11 18:02:53 +0900

    허프만 코드

--- algorithm_test_/algorithm_test_/algo_12141777_0505.cpp
+++ algorithm_test_/algorithm_test_/algo_12141777_0505.cpp
@@ -1,94 +1,94 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <stdio.h>
-#include <string.h>
-#include <vector>
-#include <algorithm>
-using namespace std;
-int next_arr[50];
-
-struct text_info {
-	char txt[10];
-	int	 pos;
-	int seq;
-};
-vector<text_info> text_vec;
-// next 배열 생성
-void Initnext_arr(char* p){
-	int i, j = 0, M = strlen(p);
-	next_arr[0] = -1;
-	for (i = 0, j = -1; i < M; i++, j++) {
-		// 개선된 방법으로 next 배열 생성
-		next_arr[i] = (p[i] == p[j]) ? next_arr[j] : j;
-		while ((j >= 0) && (p[i] != p[j]))
-			j = next_arr[j];
-	}
-}
-//next 배열을 사용하여 text에 pattern 비교
-int KMP(char *p, char *t){
-	int i, j, M = strlen(p), N = strlen(t);
-	Initnext_arr(p);
-	for (i = 0, j = 0; j < M && i < N; i++, j++)
-		while ((j >= 0) && (t[i] != p[j])) j = next_arr[j];
-	if (j == M) return i - M;
-	else return i;
-}
-// sort함수 정렬기준
-bool compare(text_info a, text_info b) {
-	// stable하게 만들기 위해 seq 비교
-	if (a.pos == b.pos) {
-		if (a.seq < b.seq) 
-			return a.pos < b.pos;
-		else
-			return a.pos > b.pos;
-	}
-	else
-		return a.pos < b.pos;
-}
-
-int main(){
-	char pattern[10] = "";
-	cin >> pattern;
-	int cnt = 0;
-	while (true) {
-		int M, N, pos, previous = 0, i = 0;
-		char text[10] = "";
-		cin >> text;
-		if (!strcmp(text, "0"))
-			break;
-		M = strlen(pattern); 
-		N = strlen(text);
-		//text에 패턴과 일치하는 곳을 찾을때까지 반복
-		while (true) {
-			// pos : 마지막 같은 문자의 위치
-			pos = KMP(pattern, text + i);
-			pos += previous;
-			i = pos + M;
-			// text에서 패턴이 발견된 위치
-			if (i <= N) {
-				text_info tmp;
-				strcpy(tmp.txt, text);
-				tmp.pos = pos;
-				tmp.seq = cnt;
-				text_vec.push_back(tmp);
-			}
-			// text에서 패턴을 찾지 못한경우
-			if (i > N) {
-				text_info tmp;
-				strcpy(tmp.txt, text);
-				tmp.pos = 10+cnt;
-				tmp.seq = cnt;
-				text_vec.push_back(tmp);
-				break;
-			}
-			else break;
-			previous = i;
-		}
-		cnt++;
-	}
-	// text_vec 정렬 실행
-	sort(text_vec.begin(), text_vec.end(), compare);
-	for (int i = 0; i < text_vec.size(); i++)
-		cout << text_vec.at(i).txt << endl;
-	return 0;
-}
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <stdio.h>
+//#include <string.h>
+//#include <vector>
+//#include <algorithm>
+//using namespace std;
+//int next_arr[50];
+//
+//struct text_info {
+//	char txt[10];
+//	int	 pos;
+//	int seq;
+//};
+//vector<text_info> text_vec;
+//// next 배열 생성
+//void Initnext_arr(char* p){
+//	int i, j = 0, M = strlen(p);
+//	next_arr[0] = -1;
+//	for (i = 0, j = -1; i < M; i++, j++) {
+//		// 개선된 방법으로 next 배열 생성
+//		next_arr[i] = (p[i] == p[j]) ? next_arr[j] : j;
+//		while ((j >= 0) && (p[i] != p[j]))
+//			j = next_arr[j];
+//	}
+//}
+////next 배열을 사용하여 text에 pattern 비교
+//int KMP(char *p, char *t){
+//	int i, j, M = strlen(p), N = strlen(t);
+//	Initnext_arr(p);
+//	for (i = 0, j = 0; j < M && i < N; i++, j++)
+//		while ((j >= 0) && (t[i] != p[j])) j = next_arr[j];
+//	if (j == M) return i - M;
+//	else return i;
+//}
+//// sort함수 정렬기준
+//bool compare(text_info a, text_info b) {
+//	// stable하게 만들기 위해 seq 비교
+//	if (a.pos == b.pos) {
+//		if (a.seq < b.seq) 
+//			return a.pos < b.pos;
+//		else
+//			return a.pos > b.pos;
+//	}
+//	else
+//		return a.pos < b.pos;
+//}
+//
+//int main(){
+//	char pattern[10] = "";
+//	cin >> pattern;
+//	int cnt = 0;
+//	while (true) {
+//		int M, N, pos, previous = 0, i = 0;
+//		char text[10] = "";
+//		cin >> text;
+//		if (!strcmp(text, "0"))
+//			break;
+//		M = strlen(pattern); 
+//		N = strlen(text);
+//		//text에 패턴과 일치하는 곳을 찾을때까지 반복
+//		while (true) {
+//			// pos : 마지막 같은 문자의 위치
+//			pos = KMP(pattern, text + i);
+//			pos += previous;
+//			i = pos + M;
+//			// text에서 패턴이 발견된 위치
+//			if (i <= N) {
+//				text_info tmp;
+//				strcpy(tmp.txt, text);
+//				tmp.pos = pos;
+//				tmp.seq = cnt;
+//				text_vec.push_back(tmp);
+//			}
+//			// text에서 패턴을 찾지 못한경우
+//			if (i > N) {
+//				text_info tmp;
+//				strcpy(tmp.txt, text);
+//				tmp.pos = 10+cnt;
+//				tmp.seq = cnt;
+//				text_vec.push_back(tmp);
+//				break;
+//			}
+//			else break;
+//			previous = i;
+//		}
+//		cnt++;
+//	}
+//	// text_vec 정렬 실행
+//	sort(text_vec.begin(), text_vec.end(), compare);
+//	for (int i = 0; i < text_vec.size(); i++)
+//		cout << text_vec.at(i).txt << endl;
+//	return 0;
+//}
--- algorithm_test_/algorithm_test_/test_main.cpp
+++ algorithm_test_/algorithm_test_/test_main.cpp
@@ -1,88 +1,88 @@
-#include <stdio.h>
-#include <string.h>
-
-int next[50];
+//#include <stdio.h>
+//#include <string.h>
 //
-
-void InitNext(char* p){
-	int i, j = 0, M = strlen(p);
-	next[0] = -1;
-	for (i = 0, j = -1; i < M; i++, j++) {
-		next[i] = j; //원래버전
-		//next[i] = (p[i] == p[j]) ? next[j] : j; //개선된버전
-		while ((j >= 0) && (p[i] != p[j]))
-			j = next[j];
-	}
-
-}
-
+//int next[50];
+////
+//
+//void InitNext(char* p){
+//	int i, j = 0, M = strlen(p);
+//	next[0] = -1;
+//	for (i = 0, j = -1; i < M; i++, j++) {
+//		next[i] = j; //원래버전
+//		//next[i] = (p[i] == p[j]) ? next[j] : j; //개선된버전
+//		while ((j >= 0) && (p[i] != p[j]))
+//			j = next[j];
+//	}
+//
+//}
+//
+////
+////int main()
+////{
+////	//char p[50] = "10100111";
+////	char p[50] = "abababca";  //abababca
+////
+////	//char p[50] = "AABAA";
+////	//char p[50] = "ABAABAB";
+////	//char p[50] = "AAAAA";
+////	//char p[50] = "abababca";
+////	int i, M = strlen(p);
+////	InitNext(p);
+////	for (i = 0; i < M; i++)
+////		printf("%d ", next[i]);
+////	printf("\n");
+////	return 0;
+////}
+//
+//
+//
+//int KMP(char *p, char *t)
+//{
+//	int i, j, M = strlen(p), N = strlen(t);
+//	InitNext(p);
+//	for (i = 0, j = 0; j < M && i < N; i++, j++)
+//		while ((j >= 0) && (t[i] != p[j])) j = next[j];
+//	if (j == M) return i - M;
+//	else return i;
+//}
 //
 //int main()
 //{
-//	//char p[50] = "10100111";
-//	char p[50] = "abababca";  //abababca
+//	//char text[100] = "1001110100101000101001110";
+//	//char pattern[50] = "10100111";
 //
-//	//char p[50] = "AABAA";
-//	//char p[50] = "ABAABAB";
-//	//char p[50] = "AAAAA";
-//	//char p[50] = "abababca";
-//	int i, M = strlen(p);
-//	InitNext(p);
-//	for (i = 0; i < M; i++)
-//		printf("%d ", next[i]);
-//	printf("\n");
+//
+//	//char text[100] = "ababababcababababcaabbabababcaab";
+//	//char pattern[50] = "abababca";
+//
+//	char text[100] = "AABAAbabcababAABAAaabbabAABAAaab";
+//	char pattern[50] = 	"AABAA";
+//	int M, N, pos, previous = 0, i = 0;
+//
+//	M = strlen(pattern); N = strlen(text);
+//	while (1) {
+//		pos = KMP(pattern, text + i);
+//		pos += previous;
+//		i = pos + M;
+//		if (i <= N) printf("패턴이 발생한 위치 : %d\n", pos);
+//		else break;
+//		previous = i;
+//	}
+//	printf("스트링 탐색 종료.\n");
 //	return 0;
 //}
-
-
-
-int KMP(char *p, char *t)
-{
-	int i, j, M = strlen(p), N = strlen(t);
-	InitNext(p);
-	for (i = 0, j = 0; j < M && i < N; i++, j++)
-		while ((j >= 0) && (t[i] != p[j])) j = next[j];
-	if (j == M) return i - M;
-	else return i;
-}
-
-int main()
-{
-	//char text[100] = "1001110100101000101001110";
-	//char pattern[50] = "10100111";
-
-
-	//char text[100] = "ababababcababababcaabbabababcaab";
-	//char pattern[50] = "abababca";
-
-	char text[100] = "AABAAbabcababAABAAaabbabAABAAaab";
-	char pattern[50] = 	"AABAA";
-	int M, N, pos, previous = 0, i = 0;
-
-	M = strlen(pattern); N = strlen(text);
-	while (1) {
-		pos = KMP(pattern, text + i);
-		pos += previous;
-		i = pos + M;
-		if (i <= N) printf("패턴이 발생한 위치 : %d\n", pos);
-		else break;
-		previous = i;
-	}
-	printf("스트링 탐색 종료.\n");
-	return 0;
-}
-
-/*
-next[0] = -1;
-for (int i = 1; i<=M; i++)
-{
-//if (p[i] == p[j]) next[i] = next[j];
-while (j > 0 && p[i] != p[j]) j = next[j];
-if (p[i] == p[j])
-{
-next[i + 1] = ++j;
-//printf("%c, %c\n", p[i + 1], p[j]);
-if (p[i] == p[j]) next[i+1] = next[j-1];
-}
-}
-*/
+//
+///*
+//next[0] = -1;
+//for (int i = 1; i<=M; i++)
+//{
+////if (p[i] == p[j]) next[i] = next[j];
+//while (j > 0 && p[i] != p[j]) j = next[j];
+//if (p[i] == p[j])
+//{
+//next[i + 1] = ++j;
+////printf("%c, %c\n", p[i + 1], p[j]);
+//if (p[i] == p[j]) next[i+1] = next[j-1];
+//}
+//}
+//*/
