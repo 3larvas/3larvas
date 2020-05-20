c7bf4dd2d6c987ff45834ad24d58d77bb77f5b09
3larvas <3larvas@gmail.com>
2020-05-21 06:02:13 +0900

    충돌처리

---
+++ algorithm_test_/algorithm_test_/algo_12141777_0519.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#define EMPTY	0
+#define CHEEZE	1
+#define TRAP	2
+using namespace std;
+int N, M;
+int map[101][101];
+int chz[101][101];
+
+int main() {
+	int i, j;
+	cin >> N >> M;
+	int input;
+	for (i = 0; i < N; i++) {
+		for (j = 0; j < M; j++) {
+			cin >> input;
+			map[i][j] = input;
+			chz[i][j] = 0;
+		}
+	}
+	for (i = N - 2; i >= 0; i--) {
+		if (map[i][0] == 1) {
+			chz[i][0] = chz[i + 1][0] + 1;
+		}
+		else if (map[i][0] == EMPTY) {
+			chz[i][0] = chz[i + 1][0];
+		}
+		else if (map[i][0] == TRAP) {
+			break;
+		}
+	}
+	for (j = 1; j < M; j++) {
+		if (map[N - 1][j] == CHEEZE) {
+			chz[N - 1][j] = chz[N - 1][j - 1] + 1;
+		}
+		else if (map[N - 1][j] == EMPTY) {
+			chz[N - 1][j] = chz[N - 1][j - 1];
+		}
+		else if (map[N - 1][j] == TRAP) {
+			break;
+		}
+	}
+	for (i = N - 2; i >= 0; i--) {
+		for (j = 1; j < M; j++) {
+			if (map[i][j] == CHEEZE) {
+				if (chz[i + 1][j] >= chz[i][j - 1]) {
+					chz[i][j] = chz[i + 1][j] + 1;
+				}
+				else {
+					chz[i][j] = chz[i][j - 1] + 1;
+				}
+			}
+			else if (map[i][j] == EMPTY) {
+				if (chz[i + 1][j] >= chz[i][j - 1]) {
+					chz[i][j] = chz[i + 1][j];
+				}
+				else {
+					chz[i][j] = chz[i][j - 1];
+				}
+			}
+			else if (map[i][j] == TRAP) {
+				chz[i][j] = -1;
+			}
+		}
+	}
+	cout << chz[0][M - 1];
+	return 0;
+}
+
--- algorithm_test_/algorithm_test_/sam_16236_아기상어.cpp
+++ algorithm_test_/algorithm_test_/sam_16236_아기상어.cpp
@@ -1,84 +1,84 @@
-// https://www.acmicpc.net/problem/16236
-#pragma warning(disable : 4996)
-#include <stdio.h>
-#include <queue>
-using namespace std;
-
-int n;
-int map[21][21];
-int dir_x[4] = { 0, 0, 1, -1 };
-int dir_y[4] = { 1, -1, 0, 0 };
-
-struct shark {
-	int map[21][21];
-	int lv = 2;
-	int x = 0;
-	int y = 0;
-	int cnt = 0;
-	int time = 0;
-};
-
-shark baby;
-queue<shark> q;
-int result;
-
-bool chk_fish(shark cur_baby) {
-	for (int i = 0; i < n * n; i++) {
-		if (cur_baby.map[i / n][i % n] != 0) {
-			if (cur_baby.map[i / n][i % n] == 9) continue;
-			return false;
-		}
-	}
-	return true;
-}
-
-void BFS() {
-	while (!q.empty()) {
-		shark cur_baby = q.front();
-		q.pop();
-		if (chk_fish(cur_baby)) {
-			result = cur_baby.time;
-			break;
-		}
-		for (int i = 0; i < 4; i++) {
-			int nxt_x = cur_baby.x + dir_x[i];
-			int nxt_y = cur_baby.y + dir_y[i];
-			if ((nxt_x >= 0) && (nxt_y >= 0) && (nxt_x < n) && (nxt_y < n) && (cur_baby.map[nxt_y][nxt_x]<=cur_baby.lv)) {
-				shark nxt_baby = cur_baby;
-				nxt_baby.x = nxt_x;
-				nxt_baby.y = nxt_y;
-				nxt_baby.time = cur_baby.time + 1;
-				if (cur_baby.map[nxt_y][nxt_x] >= 1 && cur_baby.map[nxt_y][nxt_x] < cur_baby.lv) {
-					nxt_baby.cnt = cur_baby.cnt + 1;
-					if (nxt_baby.cnt == nxt_baby.lv) {
-						nxt_baby.lv++;
-						nxt_baby.cnt = 0;
-					}
-				}
-				nxt_baby.map[cur_baby.y][cur_baby.x] = 0;
-				nxt_baby.map[nxt_y][nxt_x] = 9;
-				q.push(nxt_baby);
-			}
-		}
-	}
-}
-
-int main() {
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			int input;
-			scanf("%d", &input);
-			baby.map[i][j] = input;
-			if (input == 9) {
-				baby.x = j;
-				baby.y= i;
-			}
-		}
-	}
-	q.push(baby);
-	BFS();
-	printf("%d", result);
-	return 0;
-}
-
+//// https://www.acmicpc.net/problem/16236
+//#pragma warning(disable : 4996)
+//#include <stdio.h>
+//#include <queue>
+//using namespace std;
+//
+//int n;
+//int map[21][21];
+//int dir_x[4] = { 0, 0, 1, -1 };
+//int dir_y[4] = { 1, -1, 0, 0 };
+//
+//struct shark {
+//	int map[21][21];
+//	int lv = 2;
+//	int x = 0;
+//	int y = 0;
+//	int cnt = 0;
+//	int time = 0;
+//};
+//
+//shark baby;
+//queue<shark> q;
+//int result;
+//
+//bool chk_fish(shark cur_baby) {
+//	for (int i = 0; i < n * n; i++) {
+//		if (cur_baby.map[i / n][i % n] != 0) {
+//			if (cur_baby.map[i / n][i % n] == 9) continue;
+//			return false;
+//		}
+//	}
+//	return true;
+//}
+//
+//void BFS() {
+//	while (!q.empty()) {
+//		shark cur_baby = q.front();
+//		q.pop();
+//		if (chk_fish(cur_baby)) {
+//			result = cur_baby.time;
+//			break;
+//		}
+//		for (int i = 0; i < 4; i++) {
+//			int nxt_x = cur_baby.x + dir_x[i];
+//			int nxt_y = cur_baby.y + dir_y[i];
+//			if ((nxt_x >= 0) && (nxt_y >= 0) && (nxt_x < n) && (nxt_y < n) && (cur_baby.map[nxt_y][nxt_x]<=cur_baby.lv)) {
+//				shark nxt_baby = cur_baby;
+//				nxt_baby.x = nxt_x;
+//				nxt_baby.y = nxt_y;
+//				nxt_baby.time = cur_baby.time + 1;
+//				if (cur_baby.map[nxt_y][nxt_x] >= 1 && cur_baby.map[nxt_y][nxt_x] < cur_baby.lv) {
+//					nxt_baby.cnt = cur_baby.cnt + 1;
+//					if (nxt_baby.cnt == nxt_baby.lv) {
+//						nxt_baby.lv++;
+//						nxt_baby.cnt = 0;
+//					}
+//				}
+//				nxt_baby.map[cur_baby.y][cur_baby.x] = 0;
+//				nxt_baby.map[nxt_y][nxt_x] = 9;
+//				q.push(nxt_baby);
+//			}
+//		}
+//	}
+//}
+//
+//int main() {
+//	scanf("%d", &n);
+//	for (int i = 0; i < n; i++) {
+//		for (int j = 0; j < n; j++) {
+//			int input;
+//			scanf("%d", &input);
+//			baby.map[i][j] = input;
+//			if (input == 9) {
+//				baby.x = j;
+//				baby.y= i;
+//			}
+//		}
+//	}
+//	q.push(baby);
+//	BFS();
+//	printf("%d", result);
+//	return 0;
+//}
+//
