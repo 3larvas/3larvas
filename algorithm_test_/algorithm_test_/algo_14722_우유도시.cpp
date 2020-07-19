6a27203012d2f3dc6e36edd2ef512b7a445719cb
3larvas <3larvas@gmail.com>
2020-07-19 11:45:37 +0900

    우유도시 풀이 중

--- algorithm_test_/algorithm_test_/algo_14722_우유도시.cpp
+++ algorithm_test_/algorithm_test_/algo_14722_우유도시.cpp
@@ -1,15 +1,12 @@
 //√‚√≥ : https://www.acmicpc.net/problem/14722
 #pragma warning(disable : 4996)
 #include <stdio.h>
-#include <algorithm>
 #include <utility>
 
 using namespace std;
-int state, N;
+int N;
 int map[1001][1001];
 pair<int, int> dp[1001][1001];
-int dir_x[2] = { 0, 1 };
-int dir_y[2] = { 1, 0 };
 
 int main() {
 	int start_r = -1, start_c = -1;
@@ -60,33 +57,61 @@ int main() {
 			break;
 		}
 	}
-	for (int i = start_r + 1; i < N; i++) {
-		for (int j = start_c + 1; j < N; j++) {
-			int cur_val = map[i][j];
-			int pre_val_r = dp[i - 1][j].first;
-			int pre_val_c = dp[i][j-1].first;
-			int tmp_1=0, tmp_2=0;
-			switch (cur_val) {
-			case 0:
-				if (pre_val_r == 2) tmp_1 = dp[i - 1][j].second + 1;
-				if (pre_val_c == 2) tmp_2 = dp[i][j-1].second + 1;
-				if(tmp_1 != 0 || tmp_2 != 0) dp[i][j] = make_pair(cur_val, max(tmp_1, tmp_2));
-				else dp[i][j] = dp[i - 1][j];
-				break;
-			case 1:
-				if (pre_val_r == 0) tmp_1 = dp[i - 1][j].second + 1;
-				if (pre_val_c == 0) tmp_2 = dp[i][j - 1].second + 1;
-				if (tmp_1 != 0 || tmp_2 != 0) dp[i][j] = make_pair(cur_val, max(tmp_1, tmp_2));
-				else dp[i][j] = dp[i - 1][j];
-				break;
-			case 2:
-				if (pre_val_r == 1) tmp_1 = dp[i - 1][j].second + 1;
-				if (pre_val_c == 1) tmp_2 = dp[i][j - 1].second + 1;
-				if (tmp_1 != 0 || tmp_2 != 0) dp[i][j] = make_pair(cur_val, max(tmp_1, tmp_2));
-				else dp[i][j] = dp[i - 1][j];
-				break;
+	if (start_r != -1) {
+		for (int i = start_r + 1; i < N; i++) {
+			for (int j = start_c + 1; j < N; j++) {
+				int cur_val = map[i][j];
+				int pre_val_r = dp[i - 1][j].first;
+				int pre_val_c = dp[i][j - 1].first;
+				int nxt_val_r = dp[i - 1][j].first;
+				int nxt_val_c = dp[i][j - 1].first;
+				int nxt_cnt_r = dp[i - 1][j].second;
+				int nxt_cnt_c = dp[i][j - 1].second;
+
+				switch (cur_val) {
+				case 0:
+					if (pre_val_r == 2) {
+						nxt_val_r = 0;
+						nxt_cnt_r++;
+					}
+					if (pre_val_c == 2) {
+						nxt_val_c = 0;
+						nxt_cnt_c++;
+					}
+					if (nxt_cnt_r > nxt_cnt_c) dp[i][j] = make_pair(nxt_val_r, nxt_cnt_r);
+					else dp[i][j] = make_pair(nxt_val_c, nxt_cnt_c);
+					break;
+				case 1:
+					if (pre_val_r == 0) {
+						nxt_val_r = 1;
+						nxt_cnt_r++;
+					}
+					if (pre_val_c == 0) {
+						nxt_val_c = 1;
+						nxt_cnt_c++;
+					}
+					if (nxt_cnt_r > nxt_cnt_c) dp[i][j] = make_pair(nxt_val_r, nxt_cnt_r);
+					else dp[i][j] = make_pair(nxt_val_c, nxt_cnt_c);
+					break;
+				case 2:
+					if (pre_val_r == 1) {
+						nxt_val_r = 2;
+						nxt_cnt_r++;
+					}
+					if (pre_val_c == 1) {
+						nxt_val_c = 2;
+						nxt_cnt_c++;
+					}
+					if (nxt_cnt_r > nxt_cnt_c) dp[i][j] = make_pair(nxt_val_r, nxt_cnt_r);
+					else dp[i][j] = make_pair(nxt_val_c, nxt_cnt_c);
+					break;
+				}
 			}
 		}
+		printf("%d", dp[N - 1][N - 1].second);
 	}
+	else
+		printf("%d", 0);
+	
 	return 0;
 }
