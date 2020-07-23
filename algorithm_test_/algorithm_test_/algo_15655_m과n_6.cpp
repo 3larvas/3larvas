3b359e504e62e5b8d451708359aefb9ee56c775e
3larvas <3larvas@gmail.com>
2020-07-23 21:22:32 +0900

    n과m 시리즈

---
+++ algorithm_test_/algorithm_test_/algo_15655_m과n_6.cpp
@@ -0,0 +1,18 @@
+// √‚√≥ : https://www.acmicpc.net/problem/15655
+#pragma warning(disable : 4996)
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[10];
+int vis[10];
+int result[10];
+
+int main() {
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++) 
+		scanf("%d", &arr[i]);
+
+	return 0;
+}
